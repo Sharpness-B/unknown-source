0cf34169bf21658abfe9f3322f1f6e2623336ed1
Bendik Skarpnes <bendik.skarpnes@gmail.com>
2022-02-14 16:52:48 +0100

    add vailidate answer func

--- pages/api/validate-answer.js
+++ pages/api/validate-answer.js
@@ -1,4 +1,21 @@
-export default function handler(req, res) {
-  console.log(process.env.TEST)
-  res.status(200).json({ name: "process.env.TEST" })
+import { sources } from "./modules/sources"
+import Parser from "rss-parser"
+let parser = new Parser()
+
+export default async function validateAnswer(req, res) {
+    const guess = (req.query.guess || (req.body && req.body.guess))
+    const title = (req.query.title || (req.body && req.body.title))
+
+    const feed = await parser.parseURL(sources[ guess ].rssEndpoint)
+    const article = feed.items.find(e => e.title.includes(title))
+
+    const isCorrect = typeof article !== "undefined"
+
+    res.status(200).json(
+        {
+            guess: guess,
+            title: title,
+            isCorrect: isCorrect
+        }
+    )
 }
