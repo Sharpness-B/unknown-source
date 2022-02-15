7a45dd00e7101b33a8e92e302940643cd63496a1
Bendik Skarpnes <bendik.skarpnes@gmail.com>
2022-02-15 21:41:34 +0100

    implement game

--- pages/components/game.js
+++ pages/components/game.js
@@ -1,6 +1,10 @@
 import styles from '../../styles/Game.module.css'
 import homeStyles from '../../styles/Home.module.css'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, createRef } from 'react'
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
 
 async function fetchQuestion() {
     const response = await fetch("api/get-question")
@@ -26,31 +30,88 @@ function Game() {
     const [gameState, SetGameState] = useState("game")
     const [selected, SetSelected] = useState("")
 
-    const [score, SetScore] = useState(1)
+    const [score, SetScore] = useState(0)
+    const [round, SetRound] = useState(0)
+
     const [alternatives, SetAlternatives] = useState([])
     const [question, SetQuestion] = useState("")
+
+    const [elRefs, setElRefs] = useState([]);
+
+    useEffect(() => {
+        // add or remove refs
+        setElRefs((elRefs) =>
+          Array(alternatives.length)
+            .fill()
+            .map((_, i) => elRefs[i] || createRef()),
+        );
+    }, [alternatives.length]);
 
     useEffect(() => {
         fetchQuestion().then(question => {
             SetQuestion(question.title)
             SetAlternatives(question.alternatives)
         })
-    }, [])
+    }, [round])
 
     function handleClick(guess) {
+        if (gameState !== "game") return
+        SetGameState("validating")
+
+        let correctRef
         checkAnswer(question, guess)
-            .then(result => console.log(result))
+            .then(result => {
+                correctRef = elRefs[ alternatives.indexOf(result.correct) ]
+                
+                // update score
+                if (result.correct === guess) {
+                    SetScore(s => s+1)
+                }
+
+                // hilight correct
+                correctRef.current.style.color       = "#00b500"
+                correctRef.current.style.borderColor = "#00b500"
+                
+                delay(1000).then(() => {
+                    // remove hilighting
+                    correctRef.current.style.color       = null
+                    correctRef.current.style.borderColor = null
+                    
+                    // game logic
+                    if (result.correct === guess) {
+                        SetRound(s => s+1)
+                        SetGameState("game")
+                    }
+                    else {
+                        SetGameState("finished")
+                    }
+
+                })
+            })
     }
 
     return <article id={"spill"} className={styles.article}> 
-
-        <div>
-            <h2 className={styles}>{question}</h2>
-            <code className={homeStyles.code}>{question ? "Hvem har publisert denne overskriften?" : null}</code>
-            <div className={styles.sources}>
-                { alternatives.map((source, index) => <div key={index} className={homeStyles.card} onClick={()=>handleClick(source)}>{source}</div>) }
-            </div>
-        </div>
+        <code className={styles.score}>Score: {score}</code>
+        
+        {gameState !== "finished" ?
+            <>
+                <div>
+                    <h2>{question}</h2>
+                    <code className={homeStyles.code}>{question ? "Hvem har publisert denne overskriften?" : null}</code>
+                    <div className={styles.sources}>
+                        { alternatives.map((source, index) => 
+                            <div 
+                                key={index} 
+                                ref={elRefs[index]} 
+                                className={homeStyles.card} 
+                                onClick={()=>handleClick(source)}>{source}
+                            </div>) }
+                    </div>
+                </div>
+            </>
+        : 
+            null
+        }
         
     </article>
 }
