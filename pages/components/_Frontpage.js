0e6462a06e018d0973e0a0ed11c5fd71ba83ed35
Bendik Skarpnes <bendik.skarpnes@gmail.com>
2022-02-23 23:23:02 +0100

    add dynamic lanuage in frontpage component

--- pages/components/_Frontpage.js
+++ pages/components/_Frontpage.js
@@ -2,29 +2,66 @@ import styles from '../../styles/Home.module.css'
 
 function Frontpage({language}) {
   const content = {
-    no: {},
-    en: {}
+    title: {
+      no: "Ukjent Kilde",
+      en: "Unknown Source"  
+    },
+    description1: {
+      no: "Les aktuelle overskrifter fra nyhetene og gjett avisen!",
+      en: "Read current headlines from the news and guess the newspaper!" 
+    },
+    description2: {
+      no: "Hvor uavhengige er egentlig de norske mediene?",
+      en: "How biased is the news?" 
+    },
+
+    play: {
+      no: "Spill",
+      en: "Play"
+    },
+    challenge: {
+      no: "Tar du utfordringen?",
+      en: "Do you take the challenge?"
+    },
+
+    leaderboard: {
+      no: "Se topplisten",
+      en: "Leaderboard"
+    },
+    howmany: {
+      no: "Hvor mange klarte du å gjette riktig?",
+      en: "How many did you manage to guess correctly?"
+    },
+
+    sourcecode: {
+      no: "Kildekode",
+      en: "Source Code"
+    },
+    opensource: {
+      no: "Ukjent Kilde har åpen kildekode.",
+      en: "Unknown Source is open source."
+    },
   }
 
   return <main className={styles.main}>
     <h1 className={styles.title}>
-      Ukjent Kilde
+      {content.title[language]}
     </h1>
 
     <p className={styles.description}>
-      Les aktuelle overskrifter fra nyhetene og gjett avisen! <br></br>
-      Hvor uavhengige er egentlig de norske mediene?
+      {content.description1[language]} <br></br>
+      {content.description2[language]}
     </p>
 
     <div className={styles.grid}>
       <a href="#spill" className={styles.card}>
-        <h2>Spill &rarr;</h2>
-        <p>Tar du utfordringen?<br></br><br></br></p>
+        <h2>{content.play[language]} &rarr;</h2>
+        <p>{content.challenge[language]}<br></br><br></br></p>
       </a>
 
       <a href="#toppliste" className={styles.card}>
-        <h2>Se topplisten &rarr;</h2>
-        <p>Hvor mange klarte du å gjette riktig?</p>
+        <h2>{content.leaderboard[language]} &rarr;</h2>
+        <p>{content.howmany[language]}</p>
       </a>
 
       <a
@@ -33,8 +70,8 @@ function Frontpage({language}) {
         target="_blank"
         rel="noreferrer"
       >
-        <h2>Kildekode &rarr;</h2>
-        <p>Ukjent Kilde har åpen kildekode.</p>
+        <h2>{content.sourcecode[language]} &rarr;</h2>
+        <p>{content.opensource[language]}</p>
       </a>
 
       {/* <a
